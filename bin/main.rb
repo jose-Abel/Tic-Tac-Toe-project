2f9cb656da968a926081898de80566a296b59df8
joseabel <joseabel.ramirezfrontany@gmail.com>
2020-09-19 09:47:42 -0400

    adding the user interaction in the main file

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,42 @@
 #!/usr/bin/env ruby
 
-puts 'Hello world!'
+puts '------------------------------------------------------------------------------------'
+puts 'Welcome to the Great Tic Tac Toe Game in the Console, be prepared to have some fun!'
+puts '------------------------------------------------------------------------------------'
+
+puts
+
+puts 'Player 1, can you please let me know your name?'
+
+player_1 = gets.chomp()
+
+puts
+
+puts 'Player 2, now is your turn, please let me know your name?'
+
+puts
+
+player_2 = gets.chomp()
+
+puts
+
+puts "Alrighty, so #{player_1} yo'll go first, please choose spot in the board"
+
+player_1_move = gets.chomp()
+
+puts
+
+puts "Now, #{player_1}, your move is displayed on the board"
+
+puts
+
+puts "#{player_2}, is your turn now, please choose spot in the board"
+
+player_2_move = gets.chomp()
+
+puts
+
+puts "Now, #{player_2}, your move is displayed on the board"
+
+puts
+  
