9c1c296285234e2206cfc901db23cb574e5d3115
joseabel <joseabel.ramirezfrontany@gmail.com>
2020-09-26 18:01:43 -0400

    Fixed bug with repeated_name method because of safe navigation rubocop error

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,24 +1,26 @@
-module HelperMethods
-  def self.name_not_empty(player_one, player_two = false)
-    puts
-    send_message("Name can't be empty, can you please let me know a valid name?\n\n")
-    player_two == false ? player_one = gets.chomp : player_two = gets.chomp
+# rubocop : disable Style/SafeNavigation
 
-    if player_one.empty?
-      name_not_empty(player_one)
-    elsif player_two
-      name_not_empty(player_one, player_two) if player_two.length.zero?
-    elsif player_two == player_one
-      repeated_name(player_one, player_two)
+module HelperMethods
+  def self.name_not_empty(*players)
+    puts
+    puts_message("Name can't be empty, can you please let me know a valid name?\n\n")
+    players[1] ? players[1] = gets.chomp : players[0] = gets.chomp
+
+    if players[0].empty?
+      name_not_empty(players[0])
+    elsif players[1] && players[1].empty?
+      name_not_empty(players[0], players[1])
+    elsif players[1] == players[0]
+      repeated_name(players[0], players[1])
     else
-      player = player_two == false ? player_one : player_two
-      send_message("Great!, thanks #{player}\n\n")
+      player = players[1] || players[0]
+      puts_message("Great!, thanks #{player}\n\n")
       player
     end
   end
 
   def self.repeated_name(player_one, player_two)
-    send_message("Sorry #{player_two} is already taken, can you please let me know another name?\n\n")
+    puts_message("Sorry #{player_two} is already taken, can you please let me know another name?\n\n")
     player_two = gets.chomp
 
     if player_two == player_one
@@ -26,8 +28,10 @@ module HelperMethods
     elsif player_two.length.zero?
       name_not_empty(player_one, player_two)
     else
-      send_message("Awesome, thanks #{player_two}, now lets play!\n\n")
+      puts_message("Awesome, thanks #{player_two}, now lets play!\n\n")
       player_two
     end
   end
 end
+
+# rubocop : enable Style/SafeNavigation
