56386c407acfe7dae9854d42df54107c8c8a0ebc
joseabel <joseabel.ramirezfrontany@gmail.com>
2020-09-26 17:17:35 -0400

    Fix winning message displayed bug and refactor code to remove Rubocop disabling rules

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,28 +1,24 @@
-# rubocop : disable Lint/UselessAssignment
-
 module HelperMethods
   def self.name_not_empty(player_one, player_two = false)
     puts
-    puts "Name can't be empty, can you please let me know a valid name?\n\n"
+    send_message("Name can't be empty, can you please let me know a valid name?\n\n")
     player_two == false ? player_one = gets.chomp : player_two = gets.chomp
 
     if player_one.empty?
-      name_not_empty(player_one, player_two = false)
-    elsif player_two&.empty?
-      name_not_empty(player_one, player_two)
+      name_not_empty(player_one)
+    elsif player_two
+      name_not_empty(player_one, player_two) if player_two.length.zero?
     elsif player_two == player_one
       repeated_name(player_one, player_two)
     else
       player = player_two == false ? player_one : player_two
-      puts
-      puts "Great!, thanks #{player}\n\n"
+      send_message("Great!, thanks #{player}\n\n")
       player
     end
   end
 
   def self.repeated_name(player_one, player_two)
-    puts
-    puts "Sorry #{player_two} is already taken, can you please let me know another name?\n\n"
+    send_message("Sorry #{player_two} is already taken, can you please let me know another name?\n\n")
     player_two = gets.chomp
 
     if player_two == player_one
@@ -30,11 +26,8 @@ module HelperMethods
     elsif player_two.length.zero?
       name_not_empty(player_one, player_two)
     else
-      puts
-      puts "Awesome, thanks #{player_two}, now lets play!\n\n"
+      send_message("Awesome, thanks #{player_two}, now lets play!\n\n")
       player_two
     end
   end
 end
-
-# rubocop : enable Lint/UselessAssignment
--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,3 @@
-# rubocop : disable Metrics/PerceivedComplexity
-
-# rubocop : disable Metrics/CyclomaticComplexity
-
 class Player
   attr_reader :name
   attr_reader :mark
@@ -16,44 +12,73 @@ class Player
   end
 
   def winning_moves
-    horizontal = horizontal_checking
-    vertical = vertical_checking
+    horizontal = horizontal_getter
+
+    vertical = vertical_getter
+
     diagonal = diagonal_checking
-    return true if horizontal || vertical || diagonal
+
+    true if horizontal || vertical || diagonal
   end
 
   private
 
-  def horizontal_checking
+  def horizontal_getter
+    true if horizontal_checking_a || horizontal_checking_b || horizontal_checking_c
+  end
+
+  def vertical_getter
+    true if vertical_checking_one || vertical_checking_two || vertical_checking_three
+  end
+
+  def horizontal_checking_a
     if moves.include?('A1') && moves.include?('A2') && moves.include?('A3')
-      puts
-      puts "#{name} you won!"
-      true
-    elsif moves.include?('B1') && moves.include?('B2') && moves.include?('B3')
-      puts
-      puts "#{name} you won!"
-      true
-    elsif moves.include?('C1') && moves.include?('C2') && moves.include?('C3')
-      puts
-      puts "#{name} you won!"
+      puts_message("#{name} you won!")
       true
     else
       false
     end
   end
 
-  def vertical_checking
+  def horizontal_checking_b
+    if moves.include?('B1') && moves.include?('B2') && moves.include?('B3')
+      puts_message("#{name} you won!")
+      true
+    else
+      false
+    end
+  end
+
+  def horizontal_checking_c
+    if moves.include?('C1') && moves.include?('C2') && moves.include?('C3')
+      puts_message("#{name} you won!")
+      true
+    else
+      false
+    end
+  end
+
+  def vertical_checking_one
     if moves.include?('A1') && moves.include?('B1') && moves.include?('C1')
-      puts
-      puts "#{name} you won!"
+      puts_message("#{name} you won!")
       true
-    elsif moves.include?('A2') && moves.include?('B2') && moves.include?('C2')
-      puts
-      puts "#{name} you won!"
+    else
+      false
+    end
+  end
+
+  def vertical_checking_two
+    if moves.include?('A2') && moves.include?('B2') && moves.include?('C2')
+      puts_message("#{name} you won!")
       true
-    elsif moves.include?('A3') && moves.include?('B3') && moves.include?('C3')
-      puts
-      puts "#{name} you won!"
+    else
+      false
+    end
+  end
+
+  def vertical_checking_three
+    if moves.include?('A3') && moves.include?('B3') && moves.include?('C3')
+      puts_message("#{name} you won!")
       true
     else
       false
@@ -62,19 +87,13 @@ class Player
 
   def diagonal_checking
     if moves.include?('A1') && moves.include?('B2') && moves.include?('C3')
-      puts
-      puts "#{name} you won!"
+      puts_message("#{name} you won!")
       true
     elsif moves.include?('A3') && moves.include?('B2') && moves.include?('C1')
-      puts
-      puts "#{name} you won!"
+      puts_message("#{name} you won!")
       true
     else
       false
     end
   end
 end
-
-# rubocop : enable Metrics/PerceivedComplexity
-
-# rubocop : enable Metrics/CyclomaticComplexity
